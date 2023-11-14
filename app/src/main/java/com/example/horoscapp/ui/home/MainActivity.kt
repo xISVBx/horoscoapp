3bab35f40455ba8b1f7a5a65884e5562d9542302
Ivan Santiago Vasquez Ballesteros <ivansantiagovb@gmail.com>
2023-11-14 17:20:52 -0500

    initial commit

---
+++ app/src/main/java/com/example/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.horoscapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscapp.R
+import com.example.horoscapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment;
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
